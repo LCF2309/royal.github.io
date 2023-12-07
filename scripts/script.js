40ec2b5bb8b150091dd9a3b794cd1e682fcc43ca
LCF2309 <c0917445@mylambton.ca>
2023-12-07 11:48:32 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+/*
+Student Name: Rohankumar Dhuda
+File Name: script.js
+*/
+
+//Global variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src"); 
+var descriptionSource = document.getElementById("despsrc");
+
+//Hamburger menu function
+function hamburger() {
+	           var menu = document.getElementById("menu-links");
+			   var logo = document.getElementById("ffc-logo"); 
+			   if (menu.style.display === "block" && logo.style.display === "none") {
+				   menu.style.display = "none";
+				   logo.style.display = "block";
+			   } else {
+				     menu.style.display = "block";
+					 logo.style.display = "none";
+			   }
+}
+
+ //Function to display a promo code
+ function discount() {
+	                var promo = document.getElementById("special");
+					promo.firstChild.nodeValue = "Promo Code: D25START";
+					promo.style.color = "#ff0000";
+					promo.style.fontSize = "2em";
+ }
